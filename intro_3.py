c2a6beafd56498c71f5740cc1b82846dbc14e287
Orion712 <129245940+Orion712@users.noreply.github.com>
2024-12-30 10:33:23 -0500

    Maxim is the best!!!!

---
+++ intro_3.py
@@ -0,0 +1,32 @@
+print ('hello world')
+print ('THANK YOU MAXIM'
+       )
+x = 10
+x = x / 2
+pass
+a = 2
+b = 5
+
+a, b = 2, 5
+add = a + b
+multi = a * b
+
+def func():
+    pass
+# Taken from mission Empty Function
+
+# write your code here
+def func(arg):
+    return arg
+    pass
+
+
+print("Example:")
+print(func(3))
+# These "asserts" are used for self-checking
+assert func(3) == 3
+assert func("string") == "string"
+assert func(True) == True
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
+
